ae224acbef4d3f20efadb95243b50d5bc17b7732
Alende Geraldo <meubebe21@hotmail.com>
2025-05-30 03:53:34 -0300

    remove all

--- src/main/java/com/example/wallet/dto/transaction/TransactionTransferDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.wallet.dto.transaction;
-
-import com.example.wallet.dto.WalletDto;
-import com.example.wallet.dto.WalletOperationDto;
-
-public record TransactionTransferDto(WalletDto fromWallet, WalletDto toWallet, WalletOperationDto walletOperation) {
-}
